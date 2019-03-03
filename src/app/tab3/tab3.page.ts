06443ba3136d9c7e44f97334d85361229720e001
edfr10 <edfr1053@gmail.com>
2019-03-03 11:07:34 -0500

    03-guia/

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,4 +5,47 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+//Atributos
+// declara las variables de las clases
+public dato;
+
+//Constuctor
+
+//el que inicia las variable
+
+
+//Métodos
+login(){
+  this.dato = function (nombre) 
+  {
+    return "hola me llamo " + nombre;
+  };
+  console.log(this.dato("david"));
+}
+
+
+
+
+
+
+
+  
+// let nombre = function (nombre) {
+  
+//     return "hola me llamo" + nombre;
+// };
+// let hola;
+
+// saludo = nombre("david");
+
+//console.log(hola)
+
+
+
+//saludo = "hola me llamo";
+//let saludo;
+//saludo1 = mellamo("david")
+//console.log(saludo);
+
+}
